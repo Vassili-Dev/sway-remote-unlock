0a970994c38003f3d036b94937b8bdbf2cad438d
VassiliDev <NickolaiVassiliev@gmail.com>
2024-02-11 03:11:56 -0500

    Add from_u16 to status

--- src/shared/net/status.rs
+++ src/shared/net/status.rs
@@ -1,3 +1,5 @@
+use crate::errors::{RemoteUnlockError, ServerError};
+
 #[derive(Debug, Copy, Clone)]
 pub enum Status {
     Ok = 200,
@@ -20,5 +22,16 @@ impl Status {
 
     pub fn to_u16(&self) -> u16 {
         *self as u16
+    }
+
+    pub fn from_u16(code: u16) -> Result<Status, RemoteUnlockError> {
+        match code {
+            200 => Ok(Status::Ok),
+            400 => Ok(Status::BadRequest),
+            403 => Ok(Status::Forbidden),
+            404 => Ok(Status::NotFound),
+            500 => Ok(Status::InternalServerError),
+            _ => Err(ServerError::new(format!("Unknown status code: {}", code)).into()),
+        }
     }
 }
